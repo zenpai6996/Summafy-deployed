0969bf9a595f925b8d715c4be6717c6b7884938c
SRB <souhardaroybarman05@gmail.com>
2025-04-05 23:06:13 +0530

    Refactor Header component structure and improve styling for better responsiveness

--- summafy/components/common/header.tsx
+++ summafy/components/common/header.tsx
@@ -3,58 +3,62 @@ import NavLink from "@/components/common/nav-link";
 import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
 
 export default function Header() {
+
+
     return(
-    <nav className={"container flex items-center justify-between py-4 lg:py-6 px-2 mx-auto"}>
-        <div className={"flex lg:flex-1"}>
-            <NavLink href={"/"} className={"flex items-center gap-1 lg:gap-2 shrink-0"}>
-                <FileTerminal
-                    className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:text-[#8F87F1] hover:rotate-12 transition-all duration-200 ease-in-out"
-                />
-               <span className={"font-extrabold mt-2 lg:text-xl " +
-                   "text-gray-900 hover:text-[#8F87F1] transition duration-300 ease-in-out"}>Summafy
+        <header className={"sticky top-0 z-50 w-full  bg-white/0 backdrop-blur-sm transition-all"}>
+            <nav className={"container flex items-center justify-between py-4 lg:py-6 px-2 mx-auto"}>
+                <div className={"flex lg:flex-1"}>
+                    <NavLink href={"/"} className={"flex items-center gap-1 lg:gap-2 shrink-0"}>
+                        <FileTerminal
+                            className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:text-[#8F87F1] hover:rotate-12 transition-all duration-200 ease-in-out"
+                        />
+                        <span className={"font-extrabold mt-2 lg:text-xl " +
+                            "text-gray-900 hover:text-[#8F87F1] transition duration-300 ease-in-out"}>Summafy
                </span>
-            </NavLink>
-        </div>
-        <div className={"flex flex-1 justify-center gap-4" +
-            "lg:gap-12 items-center"}>
-            <NavLink className={"mr-2.5"} href={"/#pricing"}>
-                Pricing
-            </NavLink>
-            <SignedIn>
-                <NavLink href={"/dashboard"}>
-                    Your Summaries
-                </NavLink>
-            </SignedIn>
-        </div>
-        <div className={"flex lg:flex-1 justify-end"}>
-           <SignedIn>
-                    <div className={"flex gap-2 items-center"}>
-                        <NavLink href={"/upload"}>
-                            Upload a PDF
-                        </NavLink>
-                        {/*<div>Pro</div>*/}
-                        <SignedIn>
-                            <div >
-                                <UserButton
-                                    appearance={{
-                                        elements: {
-                                            userButtonAvatarBox: "w-10 h-10 lg:w-12 lg:h-12",
-                                            userButtonTrigger: "focus:shadow-none"
-                                        }
-                                    }}
-                                />
-                            </div>
-                        </SignedIn>
-                    </div>
-           </SignedIn>
-
-                <SignedOut>
-                    <NavLink href={"/sign-in"}>
-                        Sign In
                     </NavLink>
-                </SignedOut>
+                </div>
+                <div className={"flex flex-1 justify-center gap-4" +
+                    "lg:gap-12 items-center"}>
+                    <NavLink className={"mr-2.5"} href={"/#pricing"}>
+                        Pricing
+                    </NavLink>
+                    <SignedIn>
+                        <NavLink href={"/dashboard"}>
+                            Your Summaries
+                        </NavLink>
+                    </SignedIn>
+                </div>
+                <div className={"flex lg:flex-1 justify-end"}>
+                    <SignedIn>
+                        <div className={"flex gap-2 items-center"}>
+                            <NavLink href={"/upload"}>
+                                Upload a PDF
+                            </NavLink>
+                            {/*<div>Pro</div>*/}
+                            <SignedIn>
+                                <div >
+                                    <UserButton
+                                        appearance={{
+                                            elements: {
+                                                userButtonAvatarBox: "w-10 h-10 lg:w-12 lg:h-12",
+                                                userButtonTrigger: "focus:shadow-none"
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            </SignedIn>
+                        </div>
+                    </SignedIn>
 
-        </div>
-    </nav>
+                    <SignedOut>
+                        <NavLink href={"/sign-in"}>
+                            Sign In
+                        </NavLink>
+                    </SignedOut>
+
+                </div>
+            </nav>
+        </header>
     )
 }
