8ce623b0876c1a329c12034524b9e90f4bc7609a
SRB <souhardaroybarman05@gmail.com>
2025-04-12 16:22:21 +0530

    Add path alias configuration to webpack in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => {
+        // Configure path aliases
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@": path.resolve(__dirname),
+            "@/lib": path.resolve(__dirname, "lib")
+        };
+        return config;
+    }
 };
 
 export default nextConfig;
