c119c1be61961f61c13aaf7068302372dbdf0375
SRB <souhardaroybarman05@gmail.com>
2025-04-12 18:56:36 +0530

    idk

--- next.config.ts
+++ next.config.ts
@@ -8,15 +8,7 @@ const nextConfig: NextConfig = {
     eslint:{
         ignoreDuringBuilds:true,
     },
-    webpack: (config) => {
-        // Configure path aliases
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            "@": path.resolve(__dirname),
-            "@/lib": path.resolve(__dirname, "lib")
-        };
-        return config;
-    }
+   
 };
 
 export default nextConfig;
